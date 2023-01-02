122ab3052cc12143e19171dcc39e85355cda585e
rndinda <rndinda73@gmail.com>
2023-01-02 05:09:33 -0800

    New Update

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""This module instantiates an instance of the Storage will be used"""
-
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from os import getenv
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
-
-if storage_type == 'db':
-    from models.engine.db_storage import DBStorage
+if getenv("HBNB_TYPE_STORAGE") == 'db':
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from models import storage_type
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class / table model"""
+    """ State class """
     __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
+            """get a list of all related city instances
+            with state_id = to the current state id
+            """
+            cities_list = []
+
+            for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+                    cities_list.append(city)
+            return cities_list
